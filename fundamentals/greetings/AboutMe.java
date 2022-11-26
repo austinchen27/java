d92375e39652b033f88d67c58be3b1d88c866bfd
Austin <austinchen27@gmail.com>
2022-11-26 11:39:15 -0800

    java first kerm

---
+++ fundamentals/greetings/AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+  public static void main(String[] args) {
+    System.out.println("My name is Austin Chen");
+    System.out.println("I am 33 years old.");
+    System.out.println("My hometown is Fremont, CA");
+  }
+}
